3262c5646f399858d71ed1f6e883fa030473f8d9
Gloria Laoye <gloria.laoye@hotmail.co.uk>
2019-10-03 19:40:13 +0100

    created test to check whether welcome message is correct. Test failed

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -3,12 +3,21 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 
 public class BibliotecaTest {
 
     @Test
-    public void test() {
-        assertEquals(1,1);
+    public void shouldReturn() {
+        //given
+        String correctWelcomeMessage = "Welcome to BiblioTeca. Your one-stop-shop for great book titles in Bangalore!";
+        //when
+        Greeting greeting = new Greeting("");
+        String actualWelcomeMessage = greeting.getMessage();
+        //then
+        assertThat(actualWelcomeMessage, is(correctWelcomeMessage));
     }
 }
