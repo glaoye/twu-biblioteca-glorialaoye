212e2d2aefcaddb7ea416271a62168ccc12bb0fa
Gloria Laoye <gloria.laoye@hotmail.co.uk>
2019-10-06 17:27:58 +0100

    added arrays holding authors and year published

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -2,10 +2,12 @@ package com.twu.biblioteca;
 
 public class Library {
     private String[] books = new String[]{"The Adventure of Rayman", "Crash Bandicoot"};
+    private String[] authors = new String[books.length];
+    private String[] yearPublished = new String[books.length];
 
     public void showBooks(){
         for(int i =0; i<books.length; i++){
-            System.out.println(books[i]);
+            System.out.printf(books[i]);
         }
 
     }
