f4767c08b42b7cb15447491cd2b185818e4344d6
Gloria Laoye <gloria.laoye@hotmail.co.uk>
2019-10-06 15:38:53 +0100

    changed test to check for print statement

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -3,21 +3,27 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 
 public class BibliotecaTest {
-
+    
     @Test
-    public void shouldReturnCorrectWelcomeMessage() {
+    public void shouldReturnCorrectWelcomeMessage(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
         //given
-        String correctWelcomeMessage = "Welcome to BiblioTeca. Your one-stop-shop for great book titles in Bangalore!";
+        String expectedWelcomeMessage  = "Welcome to BiblioTeca. Your one-stop-shop for great book titles in Bangalore!";
         //when
         Greeting greeting = new Greeting();
-        String actualWelcomeMessage = greeting.getMessage();
+        greeting.printMessage();
         //then
-        assertThat(actualWelcomeMessage, is(correctWelcomeMessage));
+        assertThat(expectedWelcomeMessage, is(outContent.toString()));
     }
 }
