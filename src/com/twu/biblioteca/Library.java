17d7ea97332efba402e772dd05b5f1c49ad691c9
Gloria Laoye <gloria.laoye@hotmail.co.uk>
2019-10-06 17:35:48 +0100

    modified method to additionally show authors and years published in co;umns

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -2,12 +2,14 @@ package com.twu.biblioteca;
 
 public class Library {
     private String[] books = new String[]{"The Adventure of Rayman", "Crash Bandicoot"};
-    private String[] authors = new String[books.length];
-    private String[] yearPublished = new String[books.length];
+    private String[] authors = new String[]{"Amanda Adams", "Bjorn Boris"};
+    private int[] yearPublished = new int[]{1066, 2019};
+
+
 
     public void showBooks(){
         for(int i =0; i<books.length; i++){
-            System.out.printf(books[i]);
+            System.out.printf("%-50s%-50s%-50s\n",books[i],authors[i],yearPublished[i]);
         }
 
     }
