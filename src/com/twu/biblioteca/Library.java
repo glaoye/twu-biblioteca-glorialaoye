316c27d139b12d8fae37efd0fd2482f7973636df
Gloria Laoye <gloria.laoye@hotmail.co.uk>
2019-10-06 17:16:09 +0100

    added class Library containing list of books

---
+++ src/com/twu/biblioteca/Library.java
@@ -0,0 +1,9 @@
+package com.twu.biblioteca;
+
+public class Library {
+    private String[] books = new String[]{"The Adventure of Rayman", "Crash Bandicoot"};
+
+    private void showBooks(){
+        System.out.print(books);
+    }
+}
