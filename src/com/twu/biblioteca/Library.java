f5204f15b5712639fda1ace36e2b1ef5dc2cb074
Gloria Laoye <gloria.laoye@hotmail.co.uk>
2019-10-06 20:48:34 +0100

    Quit option is now added to menu

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -8,7 +8,7 @@ public class Library {
     private String[] books = new String[]{"The Adventure of Rayman", "Crash Bandicoot"};
     private String[] authors = new String[]{"Amanda Adams", "Bjorn Boris"};
     private int[] yearPublished = new int[]{1066, 2019};
-    private String[] menuOptions = new String[]{"List of books"};
+    private String[] menuOptions = new String[]{"Quit Biblioteca","List of books"};
 
     public void showWelcomeMessage(){
         System.out.print(message);
@@ -17,7 +17,7 @@ public class Library {
     public void showMenu() throws InputMismatchException {
         System.out.println("Please select an option by typing in the corresponding number.\nWhen you have finished, press the \"Enter\" key.");
         for(int i=0; i<menuOptions.length; i++){
-            System.out.println((i+1) + ". "+menuOptions[i]);
+            System.out.println((i) + ". "+menuOptions[i]);
         }
 
         Scanner scanner = new Scanner(System.in);
