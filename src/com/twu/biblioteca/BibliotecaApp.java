24209f673e24310a0cb47bc91613ea13e0d5941f
Gloria Laoye <glaoye@Glorias-MacBook-Pro.local>
2019-10-10 11:05:46 +0100

    created login method that takes user input

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,6 +5,7 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         Library library = new Library();
         library.showWelcomeMessage();
+        library.login();
         System.out.print("\n\n");
         while(true) {
             library.showMenu();
--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -5,8 +5,9 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Library {
+
     private String message = "Welcome to BiblioTeca. Your one-stop-shop for great book titles in Bangalore!";
-    private String[] menuOptions = new String[]{"Quit Biblioteca","List of books","Check-out a book", "Return a book", "List of movies"};
+    private String[] menuOptions = new String[]{"Quit Biblioteca","List of books","Check-out a book", "Return a book", "List of movies", "Check-out a movie"};
 
     private Book[] books = {new Book("Alpha","Amanda","1066"),
             new Book("Beta","Bjorn","1067"),
@@ -16,10 +17,21 @@ public class Library {
     private Movie[] movies = {new Movie("Al", "1900", "Adams", "5"),
             new Movie("Be", "1910", "Beatrice", "0"),
             new Movie("Ch", "1920", "Connor", "10")};
+    private Movie[] checkedOutMovies = {};
 
 
     public void showWelcomeMessage(){
-        System.out.print(message);
+        System.out.println(message);
+    }
+
+    public void login(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your library number:");
+        int libraryNumber = scanner.nextInt();
+        System.out.println("Please enter your password:");
+        scanner = new Scanner(System.in);
+        String password = scanner.nextLine();
+        System.out.println("Login successful!");
     }
 
     public void showMenu() throws InputMismatchException {
@@ -46,6 +58,9 @@ public class Library {
                     break;
                 case 4:
                     showMovies();
+                    break;
+                case 5:
+                    checkOutMovie();
                     break;
                 default:
                     throw new InputMismatchException();
@@ -96,6 +111,33 @@ public class Library {
         }
     }
 
+    public void checkOutMovie(){
+        System.out.println("From the list below, choose which movie you would like to check-out by pressing the corresponding number followed by the \"Enter\" key.\n" +
+                "If you would like to quit, press 0");
+        showMovies();
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int choice = scanner.nextInt();
+            Movie selectedMovie = movies[choice - 1];
+            movies = removeMovieFromArray(choice - 1, movies);
+            checkedOutMovies=addMovieToArray(selectedMovie,checkedOutMovies);
+
+
+            System.out.println("\n");
+            System.out.println("Thank you, enjoy your movie!");
+            System.out.println("\n\n");
+        }catch(InputMismatchException e){
+            System.out.println("\n");
+            System.out.println("Sorry, that movie is not available!");
+            System.out.println("\n\n");
+        }catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("\n");
+            System.out.println("Sorry, that movie is not available!");
+            System.out.println("\n\n");
+        }
+    }
+
+
     public void returnBook(){
         if(checkedOutBooks.length==0){
             System.out.println("You have no checked out books to return.");
@@ -132,8 +174,26 @@ public class Library {
         }
         return newArray;
     }
+    private static Movie[] removeMovieFromArray(int index, Movie[] array){
+
+        Movie[] newArray = new Movie[array.length-1];
+        for(int i=0, k=0; i<array.length;i++){
+            if(i==index){
+                continue;
+            }
+            newArray[k]=array[i];
+            k++;
+        }
+        return newArray;
+    }
+
 
     public static Book[] addToArray(Book addition, Book[] array){
+        array = Arrays.copyOf(array, array.length + 1);
+        array[array.length - 1] = addition;
+        return array;
+    }
+    public static Movie[] addMovieToArray(Movie addition, Movie[] array){
         array = Arrays.copyOf(array, array.length + 1);
         array[array.length - 1] = addition;
         return array;
